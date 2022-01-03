bbe265d0433c6631df7e8e9c4cf25756fba59fe4
dcyoung <david@questionablyartificial.com>
2022-01-02 17:30:38 -0800

    Changes for relative import paths

--- src/components/ground.js
+++ src/components/ground.js
@@ -3,11 +3,17 @@ import * as THREE from "three";
 
 function Ground(props) {
   const size = 250;
-  const [floor, normal] = useTexture(["/surface.jpg", "/surface_normal.jpg"], {
-    wrapS: THREE.MirroredRepeatWrapping,
-    wrapT: THREE.MirroredRepeatWrapping,
-    repeat: [size / 8, size / 8],
-  });
+  const [floor, normal] = useTexture(
+    [
+      process.env.PUBLIC_URL + "/surface.jpg",
+      process.env.PUBLIC_URL + "/surface_normal.jpg",
+    ],
+    {
+      wrapS: THREE.MirroredRepeatWrapping,
+      wrapT: THREE.MirroredRepeatWrapping,
+      repeat: [size / 8, size / 8],
+    }
+  );
   return (
     <Reflector resolution={1024} args={[size, size]} {...props}>
       {(Material, props) => (

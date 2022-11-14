aaef96665044e62784080b09ba4cf6bedb698bec
dcyoung <david@ripcord.com>
2022-11-14 19:51:30 +0000

    Deploying to gh-pages from @ dcyoung/r3f-audio-visualizer@667de4d996143046461b200af501c27d1a471a48 🚀

---
+++ assets/sphere.59665948.js
@@ -0,0 +1,1 @@
+import{B as s,M as i,g as l}from"./baseSphere.b6cd0ad6.js";import{i as n}from"./utils.2902ea37.js";import{j as p}from"./index.6b7923b4.js";import"./Lut.3b31f407.js";const d=({dataRef:r,amplitude:o=1})=>p(s,{getValueForNormalizedCoord:(e,t,m=i)=>{const a=l(e,t,m);return o*n(r==null?void 0:r.current,a)}});export{d as default};
---
+++ assets/utils.2902ea37.js
@@ -0,0 +1,1 @@
+const n=(e,o)=>{if(e===void 0||!e||e.length===0)return 0;let l=o*(e.length-1),t=e[Math.floor(l)],i=e[Math.ceil(l)];return t+l%1*(i-t)};export{n as i};

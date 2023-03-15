3c5c19d036a340b0fa3a510e088d50677d6c5b06
dcyoung <david@ripcord.com>
2023-03-14 20:41:02 -0700

    Adds dedicated play on ios

--- src/components/audio/sourceControls/file.tsx
+++ src/components/audio/sourceControls/file.tsx
@@ -1,11 +1,12 @@
 import { folder, useControls } from "leva";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { audioFileInput } from "../../levaPlugins/audioFileInput";
-import { AudioSourceControlsProps } from "./common";
-
+import { AudioSourceControlsProps, iOS } from "./common";
+import "./overlay.css";
 const FileAudioControls = ({
   audio,
 }: AudioSourceControlsProps): JSX.Element => {
+  const [isPlaying, setIsPlaying] = useState(false);
   const { audioFile } = useControls({
     Audio: folder({
       audioFile: audioFileInput({ file: undefined }),
@@ -22,20 +23,60 @@ const FileAudioControls = ({
       return;
     }
     audio.src = URL.createObjectURL(audioFile);
-    const promise = audio.play();
-    if (promise !== undefined) {
-      promise
-        .then(() => console.log(`Playing ${audioFile.name}`))
-        .catch((error) => {
-          // Auto-play was prevented
-          console.error(`Error playing ${audioFile}`, error);
-        });
+    if (!iOS()) {
+      // Can play immediately on non-ios platforms
+      const promise = audio.play();
+      if (promise !== undefined) {
+        promise
+          .then(() => {
+            setIsPlaying(true);
+            console.log(`Playing ${audioFile.name}`);
+          })
+          .catch((error) => {
+            // Auto-play was prevented
+            console.error(`Error playing ${audioFile}`, error);
+          });
+      }
     }
     return () => {
       audio.pause();
+      setIsPlaying(false);
     };
   }, [audio, audioFile]);
-  return <></>;
+
+  // IOS needs a dedicated play button
+  return iOS() ? (
+    <div
+      id="info"
+      style={{ bottom: "0.5em", right: "0.5em" }}
+      hidden={!audioFile || isPlaying}
+    >
+      <button
+        disabled={!audioFile || isPlaying}
+        onClick={() => {
+          if (!audioFile) {
+            return;
+          }
+          const promise = audio.play();
+          if (promise !== undefined) {
+            promise
+              .then(() => {
+                setIsPlaying(true);
+                console.log(`Playing ${audioFile.name}`);
+              })
+              .catch((error) => {
+                // Auto-play was prevented
+                console.error(`Error playing ${audioFile}`, error);
+              });
+          }
+        }}
+      >
+        Play
+      </button>
+    </div>
+  ) : (
+    <></>
+  );
 };
 
 export default FileAudioControls;
